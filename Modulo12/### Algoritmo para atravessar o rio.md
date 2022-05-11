210d6aee925b8aa535e188fc9d4d406e49f52ec8
Alexandre R <neoindicedolar@gmail.com>
2022-05-11 14:40:14 -0300

    testandorepositorio

---
+++ Modulo12/### Algoritmo para atravessar o rio.md
@@ -0,0 +1,8 @@
+### Algoritmo para atravessar o rio
+1. Inicio;
+2. Atravessar a Cabra;
+3. Atravessar o Lobo;
+4. Trazer a Cabra de volta;
+5. Atravessar a Alfafa;
+6. Atravessar a Cabra;
+7. Fim;
---
+++ Modulo13/contasvariaveis.js
@@ -0,0 +1,21 @@
+var branco = “preto”;
+var preto = “cinza”;
+var cinza = “branco”;
+var carro = “preto”;
+var valor = 30000;
+var prestacao = 750;
+
+
+a)branco == “branco” ----- false
+
+b)  branco == cinza ---- false
+
+c)  carro === branco ---- true
+
+d)  var cavalo = carro == “preto” ? “cinza” : “marron”; --- cinza
+
+e)  Quantas prestações são necessárias para pagar o valor do carro com uma entrada
+de 3.000? Demonstre a operação. ----var qtd_prestacoes = valor/prestacao 
+                                    qtd_prestacoes
+f)  Somando as variáveis de cores é formada uma string de quantos caracteres? ---- pretocinzabranco(16)
+
